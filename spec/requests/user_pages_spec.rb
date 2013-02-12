8d01d5e58c6652f64bf6a195b6a4369286f384bf
grahamcrackers <grogers12487@gmail.com>
2013-02-11 20:22:11 -0600

    Section 7.1.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,8 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
-    before { visit signup_path }
+  	let(:user) { FactoryGirl.create(:user) }
+    before { visit signup_path(user) }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
